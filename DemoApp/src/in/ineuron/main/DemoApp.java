47e1602cf84afe6aaae6c7e3cf090141dfd732ff
2006210 <2006210@K-H-2006210-L.Kfintech.com>
2024-05-02 09:36:48 +0530

    Second Commit

--- DemoApp/src/in/ineuron/main/DemoApp.java
+++ DemoApp/src/in/ineuron/main/DemoApp.java
@@ -5,6 +5,7 @@ public class DemoApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hello world");
+		System.out.print("Hello world");
 	}
 
 }
